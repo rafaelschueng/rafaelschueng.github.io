8c4328166b5cb90ef3b28725ef71bb2ca5b73449
Rafael Schüng <rafaelschueng@nostromo.lan>
2020-06-11 23:31:00 -0300

    Criação da página de informações.

--- assets/js/presentation.js
+++ assets/js/presentation.js
@@ -1,30 +1,44 @@
 //sleep
 let sleep = (time) => new Promise ( resolve => setTimeout (resolve, time));
 
+
 async function PresentationAnimation (interval) {
 
-  let screen = document.getElementById("presentation-screen");
-  let childs = document.getElementById("presentation-words").children;
   
-  $("#presentation-screen").animate({opacity:1}, {duration:500})
+  let childs = document.getElementById("Presentation").children;
+  
+  $("#Presentation").animate({opacity:1}, {duration:750})
   await sleep(100);
 
   for(i = 0; i < childs.length; i++) {
     childs[i].style.display = "block"
     await sleep (interval)
-    if(i === childs.length - 1){
-      $("#presentation-screen").animate({opacity:0}, {duration:500})
+    if(i === (childs.length - 1)){
+      $("#About").css('display',"grid");
+      $('#Topbar').css('display','inline-flex');
+      //fade-out presentation
+      $("#Presentation").animate({opacity:0}, {duration:1000});
+      $("#Presentation").css("display", "none");
       await sleep(500);
+      //fade-in about and topbar
+      $('#About').animate({opacity:1},{duration:1000});
+      $('#Topbar').animate({opacity:1},{duration:2000})
+      //after all set body overflow
+      $('body').css('overflow', 'auto')
     }
-    childs[i].style.display = "none"
+    childs[i].style.display = "none";
   }
   await sleep(100)
-  $("#presentation-screen").removeClass("d-flex").addClass("d-none")
-  $("body").css("overflow","auto")
-  $("#main-screen").animate({margin:0 ,opacity:1},{duration : 750})
 }
 
 $(document).ready(() => {
   console.log("Ready...")
+
+  //initialize all popover
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+  //On start apresentation
   PresentationAnimation (1000);
 })
